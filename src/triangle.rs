729c9154027228332fe42fd2a16cdc5cc6aa3923
JT Raber <JMANN240raber@gmail.com>
2026-01-14 08:00:42 -0500

    on_canvas_points

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,5 +1,6 @@
 use std::convert::identity;
 
+use itertools::Itertools;
 use line_clipping::{LineSegment, Point, Window, cohen_sutherland::clip_line};
 use ratatui_core::style::Color;
 use ratatui_widgets::canvas::{Painter, Shape};
@@ -12,6 +13,19 @@ pub struct Triangle {
 impl Triangle {
     pub const fn new(coords: [(f64, f64); 3], color: Color) -> Self {
         Self { coords, color }
+    }
+
+    fn on_canvas_points(&self, bounds: (&[f64; 2], &[f64; 2])) -> Vec<(f64, f64)> {
+        let maybe_line_1 = Self::clip_line(self.coords[0], self.coords[1], bounds);
+        let maybe_line_2 = Self::clip_line(self.coords[1], self.coords[2], bounds);
+        let maybe_line_3 = Self::clip_line(self.coords[2], self.coords[0], bounds);
+
+        [maybe_line_1, maybe_line_2, maybe_line_3]
+            .into_iter()
+            .filter_map(identity)
+            .flat_map(|line| [(line.p1.x, line.p1.y), (line.p2.x, line.p2.y)].into_iter())
+            .unique_by(|(l, r)| (l.to_bits(), r.to_bits()))
+            .collect()
     }
 
     fn bounding_box_canvas(&self, bounds: (&[f64; 2], &[f64; 2])) -> Option<(f64, f64, f64, f64)> {
@@ -54,7 +68,9 @@ impl Triangle {
             .filter_map(identity)
             .max_by(|l, r| l.partial_cmp(r).unwrap());
 
-        if let (Some(x_min), Some(x_max), Some(y_min), Some(y_max)) = (maybe_x_min, maybe_x_max, maybe_y_min, maybe_y_max) {
+        if let (Some(x_min), Some(x_max), Some(y_min), Some(y_max)) =
+            (maybe_x_min, maybe_x_max, maybe_y_min, maybe_y_max)
+        {
             Some((x_min, x_max, y_min, y_max))
         } else {
             None
@@ -62,21 +78,22 @@ impl Triangle {
     }
 
     fn bounding_box_grid(&self, painter: &Painter) -> Option<(usize, usize, usize, usize)> {
-        self.bounding_box_canvas(painter.bounds()).and_then(|bounds| {
-            let maybe_p_min = painter.get_point(bounds.0, bounds.2);
+        self.bounding_box_canvas(painter.bounds())
+            .and_then(|bounds| {
+                let maybe_p_min = painter.get_point(bounds.0, bounds.2);
 
-            let Some(p_min) = maybe_p_min else {
-                return None;
-            };
+                let Some(p_min) = maybe_p_min else {
+                    return None;
+                };
 
-            let maybe_p_max = painter.get_point(bounds.1, bounds.3);
+                let maybe_p_max = painter.get_point(bounds.1, bounds.3);
 
-            let Some(p_max) = maybe_p_max else {
-                return None;
-            };
+                let Some(p_max) = maybe_p_max else {
+                    return None;
+                };
 
-            Some((p_min.0, p_max.0, p_max.1, p_min.1))
-        })
+                Some((p_min.0, p_max.0, p_max.1, p_min.1))
+            })
     }
 
     fn clip_line(
@@ -110,6 +127,20 @@ mod tests {
     use super::*;
     use ratatui_core::{buffer::Buffer, layout::Rect, style::Style, widgets::Widget};
     use ratatui_widgets::canvas::Canvas;
+
+    #[test]
+    fn test_on_canvas_points() {
+        let triangle = Triangle::new([(0.0, 0.0), (2.0, 0.0), (0.0, 1.0)], Color::White);
+
+        let zero_two_points = triangle.on_canvas_points((&[0.0, 2.0], &[0.0, 2.0]));
+        assert_eq!(zero_two_points, &[(0.0, 0.0), (2.0, 0.0), (0.0, 1.0)]);
+
+        let zero_one_points = triangle.on_canvas_points((&[0.0, 1.0], &[0.0, 1.0]));
+        assert_eq!(zero_one_points, &[(0.0, 0.0), (1.0, 0.0), (1.0, 0.5), (0.0, 1.0)]);
+
+        let six_seven_points = triangle.on_canvas_points((&[6.0, 7.0], &[6.0, 7.0]));
+        assert_eq!(six_seven_points, &[]);
+    }
 
     #[test]
     fn test_bounding_box_canvas_1() {
