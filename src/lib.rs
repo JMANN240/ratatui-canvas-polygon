ef4af4627f6f9525b7546452a23b341bb6864478
JT Raber <JMANN240raber@gmail.com>
2026-01-14 09:21:19 -0500

    try angle

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 pub mod hollow_polygon;
 pub mod triangle;
 
-fn signed_triangle_area(a: (f64, f64), b: (f64, f64), c: (f64, f64)) -> f64 {
-    0.5 * ((b.0 - a.0) * (c.1 - a.1) - (b.1 - a.1) * (c.0 - a.0))
+fn signed_area(points: &[(f64, f64)]) -> f64 {
+    assert!(points.len() >= 3);
+
+    let mut sum = 0.0;
+
+    for i in 0..points.len() {
+        let p = points[i];
+        let q = points[(i + 1) % points.len()];
+        sum += p.0 * q.1 - q.0 * p.1;
+    }
+
+    0.5 * sum
 }
 
 #[cfg(test)]
@@ -10,17 +20,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_signed_triangle_area() {
-        assert_eq!(signed_triangle_area((0.0, 0.0), (1.0, 0.0), (0.0, 1.0)), 0.5);
-        assert_eq!(signed_triangle_area((0.0, 0.0), (0.0, 1.0), (1.0, 0.0)), -0.5);
+    fn test_signed_area() {
+        assert_eq!(signed_area(&[(0.0, 0.0), (1.0, 0.0), (0.0, 1.0)]), 0.5);
+        assert_eq!(signed_area(&[(0.0, 0.0), (0.0, 1.0), (1.0, 0.0)]), -0.5);
 
-        assert_eq!(signed_triangle_area((0.0, 0.0), (2.0, 0.0), (0.0, 2.0)), 2.0);
-        assert_eq!(signed_triangle_area((0.0, 0.0), (0.0, 2.0), (2.0, 0.0)), -2.0);
+        assert_eq!(signed_area(&[(0.0, 0.0), (2.0, 0.0), (0.0, 2.0)]), 2.0);
+        assert_eq!(signed_area(&[(0.0, 0.0), (0.0, 2.0), (2.0, 0.0)]), -2.0);
 
-        assert_eq!(signed_triangle_area((-1.0, -1.0), (1.0, -1.0), (-1.0, 1.0)), 2.0);
-        assert_eq!(signed_triangle_area((-1.0, -1.0), (-1.0, 1.0), (1.0, -1.0)), -2.0);
+        assert_eq!(signed_area(&[(-1.0, -1.0), (1.0, -1.0), (-1.0, 1.0)]), 2.0);
+        assert_eq!(signed_area(&[(-1.0, -1.0), (-1.0, 1.0), (1.0, -1.0)]), -2.0);
 
-        assert_eq!(signed_triangle_area((0.0, 0.0), (2.0, 0.0), (0.0, 4.0)), 4.0);
-        assert_eq!(signed_triangle_area((0.0, 0.0), (0.0, 4.0), (2.0, 0.0)), -4.0);
+        assert_eq!(signed_area(&[(0.0, 0.0), (2.0, 0.0), (0.0, 4.0)]), 4.0);
+        assert_eq!(signed_area(&[(0.0, 0.0), (0.0, 4.0), (2.0, 0.0)]), -4.0);
     }
 }
--- src/triangle.rs
+++ src/triangle.rs
@@ -1,9 +1,9 @@
-use std::convert::identity;
-
 use itertools::Itertools;
 use line_clipping::{LineSegment, Point, Window, cohen_sutherland::clip_line};
 use ratatui_core::style::Color;
 use ratatui_widgets::canvas::{Painter, Shape};
+
+use crate::signed_area;
 
 pub struct Triangle {
     pub coords: [(f64, f64); 3],
@@ -11,8 +11,22 @@ pub struct Triangle {
 }
 
 impl Triangle {
-    pub const fn new(coords: [(f64, f64); 3], color: Color) -> Self {
-        Self { coords, color }
+    pub fn new(coords: [(f64, f64); 3], color: Color) -> Self {
+        let mut first = Self { coords, color };
+
+        if !first.is_wound_ccw() {
+            first.coords.reverse();
+        }
+
+        first
+    }
+
+    fn signed_area(&self) -> f64 {
+        signed_area(&self.coords)
+    }
+
+    fn is_wound_ccw(&self) -> bool {
+        self.signed_area().is_sign_positive()
     }
 
     fn on_canvas_points(&self, bounds: (&[f64; 2], &[f64; 2])) -> Vec<(f64, f64)> {
@@ -22,10 +36,39 @@ impl Triangle {
 
         [maybe_line_1, maybe_line_2, maybe_line_3]
             .into_iter()
-            .filter_map(identity)
+            .flatten()
             .flat_map(|line| [(line.p1.x, line.p1.y), (line.p2.x, line.p2.y)].into_iter())
             .unique_by(|(l, r)| (l.to_bits(), r.to_bits()))
             .collect()
+    }
+
+    fn on_grid_points(&self, painter: &Painter) -> Vec<(usize, usize)> {
+        self.on_canvas_points(painter.bounds())
+            .iter()
+            .filter_map(|canvas_point| painter.get_point(canvas_point.0, canvas_point.1))
+            .collect()
+    }
+
+    fn on_grid_edges(&self, painter: &Painter) -> Vec<((usize, usize), (usize, usize))> {
+        let points = self.on_grid_points(painter);
+        let mut rotated_points = points.clone();
+        rotated_points.rotate_right(1);
+        points.into_iter().zip(rotated_points).collect()
+    }
+
+    fn grid_point_in_self(&self, painter: &Painter, grid_point: (usize, usize)) -> bool {
+        self.on_grid_edges(painter)
+            .into_iter()
+            .map(|(p1, p2)| {
+                let area = signed_area(&[
+                    (p1.0 as f64, p1.1 as f64),
+                    (p2.0 as f64, p2.1 as f64),
+                    (grid_point.0 as f64, grid_point.1 as f64),
+                ]);
+
+                area.is_sign_positive()
+            })
+            .all_equal()
     }
 
     fn bounding_box_canvas(&self, bounds: (&[f64; 2], &[f64; 2])) -> Option<(f64, f64, f64, f64)> {
@@ -50,22 +93,22 @@ impl Triangle {
 
         let maybe_x_min = [maybe_line_1_x_min, maybe_line_2_x_min, maybe_line_3_x_min]
             .into_iter()
-            .filter_map(identity)
+            .flatten()
             .min_by(|l, r| l.partial_cmp(r).unwrap());
 
         let maybe_x_max = [maybe_line_1_x_max, maybe_line_2_x_max, maybe_line_3_x_max]
             .into_iter()
-            .filter_map(identity)
+            .flatten()
             .max_by(|l, r| l.partial_cmp(r).unwrap());
 
         let maybe_y_min = [maybe_line_1_y_min, maybe_line_2_y_min, maybe_line_3_y_min]
             .into_iter()
-            .filter_map(identity)
+            .flatten()
             .min_by(|l, r| l.partial_cmp(r).unwrap());
 
         let maybe_y_max = [maybe_line_1_y_max, maybe_line_2_y_max, maybe_line_3_y_max]
             .into_iter()
-            .filter_map(identity)
+            .flatten()
             .max_by(|l, r| l.partial_cmp(r).unwrap());
 
         if let (Some(x_min), Some(x_max), Some(y_min), Some(y_max)) =
@@ -80,17 +123,8 @@ impl Triangle {
     fn bounding_box_grid(&self, painter: &Painter) -> Option<(usize, usize, usize, usize)> {
         self.bounding_box_canvas(painter.bounds())
             .and_then(|bounds| {
-                let maybe_p_min = painter.get_point(bounds.0, bounds.2);
-
-                let Some(p_min) = maybe_p_min else {
-                    return None;
-                };
-
-                let maybe_p_max = painter.get_point(bounds.1, bounds.3);
-
-                let Some(p_max) = maybe_p_max else {
-                    return None;
-                };
+                let p_min = painter.get_point(bounds.0, bounds.2)?;
+                let p_max = painter.get_point(bounds.1, bounds.3)?;
 
                 Some((p_min.0, p_max.0, p_max.1, p_min.1))
             })
@@ -116,7 +150,9 @@ impl Shape for Triangle {
 
         for x in x_min..x_max {
             for y in y_min..y_max {
-                painter.paint(x, y, self.color);
+                if self.grid_point_in_self(painter, (x, y)) {
+                    painter.paint(x, y, self.color);
+                }
             }
         }
     }
@@ -136,7 +172,10 @@ mod tests {
         assert_eq!(zero_two_points, &[(0.0, 0.0), (2.0, 0.0), (0.0, 1.0)]);
 
         let zero_one_points = triangle.on_canvas_points((&[0.0, 1.0], &[0.0, 1.0]));
-        assert_eq!(zero_one_points, &[(0.0, 0.0), (1.0, 0.0), (1.0, 0.5), (0.0, 1.0)]);
+        assert_eq!(
+            zero_one_points,
+            &[(0.0, 0.0), (1.0, 0.0), (1.0, 0.5), (0.0, 1.0)]
+        );
 
         let six_seven_points = triangle.on_canvas_points((&[6.0, 7.0], &[6.0, 7.0]));
         assert_eq!(six_seven_points, &[]);
@@ -171,7 +210,11 @@ mod tests {
             .y_bounds([0.0, 1.0])
             .paint(|context| {
                 context.draw(&Triangle::new(
-                    [(0.25, 0.25), (0.25, 0.75), (0.75, 0.75)],
+                    [(0.25, 0.25), (0.25, 0.75), (0.5, 0.75)],
+                    Color::White,
+                ));
+                context.draw(&Triangle::new(
+                    [(0.75, 0.25), (0.75, 0.75), (0.5, 0.75)],
                     Color::White,
                 ));
             });
@@ -182,15 +225,16 @@ mod tests {
 
         let mut expected = Buffer::with_lines(vec![
             "                                                  ",
-            "            ⢸⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣉⣉⣉⡩⠭⠭⠭⠛⠛⠛⠋⠉⠁            ",
-            "            ⢸⣀⡠⠤⠤⠤⠒⠒⠒⠊⠉⠉⠉                         ",
+            "            ⢸⣿⣿⣿⣿⣿⣿⣿⠿⠟⠛⠋⠉⠉⠉⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿             ",
+            "            ⠸⠿⠟⠛⠋⠉              ⠉⠉⠛⠻⠿             ",
             "                                                  ",
         ]);
 
         let line_style = Style::default().white();
 
-        expected.set_style(Rect::new(12, 1, 26, 1), line_style);
-        expected.set_style(Rect::new(12, 2, 13, 1), line_style);
+        expected.set_style(Rect::new(12, 1, 25, 1), line_style);
+        expected.set_style(Rect::new(12, 2, 6, 1), line_style);
+        expected.set_style(Rect::new(32, 2, 5, 1), line_style);
 
         assert_eq!(buf, expected);
     }
